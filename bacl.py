eb5edb0ea7be1129eed601c13d5f23e891a0615b
pannac <68177486+carromcentral@users.noreply.github.com>
2026-03-10 21:14:43 -0700

    Ask question page

--- bacl.py
+++ bacl.py
@@ -3,6 +3,7 @@ import os
 import google.generativeai as genai
 
 home_page = st.Page("home.py", title="Home")
-pg = st.navigation([home_page])
+ask_question_page = st.Page("ask_question.py", title="Ask a Question")
+pg = st.navigation([home_page, ask_question_page])
 st.set_page_config(page_title="BACL 2026: Season 1")
 pg.run()
