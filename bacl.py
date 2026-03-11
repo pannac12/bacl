27dd64624e6ae0e511bd154ff46ad1ef7f52088c
pannac <68177486+carromcentral@users.noreply.github.com>
2026-03-10 21:28:45 -0700

    Renamed home to Leaderboard

--- bacl.py
+++ bacl.py
@@ -2,8 +2,8 @@ import streamlit as st
 import os
 import google.generativeai as genai
 
-home_page = st.Page("home.py", title="Home")
+leaderboard_page = st.Page("leaderboard.py", title="Leaderboard")
 ask_question_page = st.Page("ask_question.py", title="Ask a Question")
-pg = st.navigation([home_page, ask_question_page])
+pg = st.navigation([leaderboard_page, ask_question_page])
 st.set_page_config(page_title="BACL 2026: Season 1")
 pg.run()
